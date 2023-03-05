eaed9c4c130f145fbfc4f861c2b5c2718305a410
Omar Eltouny <60698807+OmarEltouny78@users.noreply.github.com>
2023-03-05 12:18:26 +0000

    StackQueueLab

---
+++ src/LinkedQueue.java
@@ -0,0 +1,26 @@
+public class LinkedQueue<E> implements Queue<E> {
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void enqueue(E e) {
+
+    }
+
+    @Override
+    public E first() {
+        return null;
+    }
+
+    @Override
+    public E dequeue() {
+        return null;
+    }
+}
